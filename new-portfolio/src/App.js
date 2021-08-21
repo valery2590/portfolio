0e4e30cfdb3144f57834cb03580ee555652e57f1
valery2590 <valeryfigueroah@gmail.com>
2021-08-21 23:10:35 +0200

    new landed

--- new-portfolio/src/App.js
+++ new-portfolio/src/App.js
@@ -7,14 +7,9 @@ import QuestionsPage from './pages/QuestionsPage';
 function App() {
   return (
     <div className="App">
-    <Router>
-          <Route exact path="/">
-              <Landing/>    
-          </Route>
-          <Route exact path="/myhistory">
-              <QuestionsPage/>
-          </Route>
-    </Router>
+    <Router> <Landing/>    </Router>
+             
+       
     </div>
   );
 }
