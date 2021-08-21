da01049cd8d4ec420cc99b8b1db40e9a2f8d2970
valery2590 <valeryfigueroah@gmail.com>
2021-08-21 21:17:16 +0200

    changed new

--- new-portfolio/src/App.js
+++ new-portfolio/src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Lourdes deja de beber, no es sano</h1>
+        <p> en un mundo de borrachos...¿cómo dejar de beber?</p>
+
+        <h2>Piensalo...</h2>
     </div>
   );
 }
