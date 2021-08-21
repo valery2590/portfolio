6bb56b1ac2c546379f2a1fb666653681bb065459
valery2590 <valeryfigueroah@gmail.com>
2021-08-21 22:23:56 +0200

    question page added

--- new-portfolio/src/App.js
+++ new-portfolio/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './pages/LandingPage';
+import QuestionsPage from './pages/QuestionsPage';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Landing/>
+      <Switch>
+          <Route exact path="/">
+              <Landing/>    
+          </Route>
+          <Route exact path="/myhistory">
+              <QuestionsPage/>
+          </Route>
+        
+        </Switch>
       </Router>
     </div>
   );
