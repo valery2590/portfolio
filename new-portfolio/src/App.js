21653487aa31c608878d2a3ea815a67401137655
valery2590 <valeryfigueroah@gmail.com>
2021-08-22 19:17:41 +0200

    new info added

--- new-portfolio/src/App.js
+++ new-portfolio/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './pages/LandingPage';
 import QuestionsPage from './pages/QuestionsPage';
