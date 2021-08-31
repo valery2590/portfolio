5f30231d47cb09ca4e2b73981a75d23bdf5539a4
valery2590 <valeryfigueroah@gmail.com>
2021-08-31 11:26:02 +0200

    contact modified

--- new-portfolio/src/components/contact/Contact.js
+++ new-portfolio/src/components/contact/Contact.js
@@ -4,9 +4,12 @@ import Logo from "../logo/Logo"
 const Contact = ()=>{
     return(
             <div className="contact__container">
-                    <div className="email_contact_section">Email</div>
-                    <div className="logo_contact_section"><Logo style="logo_footer" /></div>
-                    <div className="phone_contact_section">Phone</div>
+                <div className="contact_items">
+                    <div className="email_contact_section"><p>Email</p></div>
+                    <div className="logo_contact_section"><Logo style="logo_footer"/></div>
+                    <div className="phone_contact_section"><p>Phone</p></div>
+                </div>
+                    
             </div>
     )
 }
