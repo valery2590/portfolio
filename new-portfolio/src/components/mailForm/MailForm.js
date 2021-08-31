ce04b3b5e893398790092552029d6ceebbede9fc
valery2590 <valeryfigueroah@gmail.com>
2021-08-31 15:14:56 +0200

    send email added

--- new-portfolio/src/components/mailForm/MailForm.js
+++ new-portfolio/src/components/mailForm/MailForm.js
@@ -5,21 +5,21 @@ import "./MailForm.css"
 
 const MailForm = () => {
 
-    // function sendEmail(e){
-    //     e.preventDefault();
+    function sendEmail(e){
+        e.preventDefault();
 
-    //     emailjs.sendForm('service_lausod2', 'template_abu8jdt', e.target, 'user_iWFLiJoHRNUY2yoUbWCKE')
-    //     .then((result) => {
-    //         console.log(result.text);
-    //     }, (error) => {
-    //         console.log(error.text);
-    //     });
-    //     e.target.reset();
-    // }
+        emailjs.sendForm('service_lausod2', 'template_abu8jdt', e.target, 'user_iWFLiJoHRNUY2yoUbWCKE')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    }
 
     return (
         <div className="Mailform__container">
-            <form  className="form__container">
+            <form onSubmit={sendEmail} className="form__container">
                 <p className="form_title">Please fill in to contact me</p>
                 <input type="text"  placeholder="Your Name" name="name" className="form_input"/>
                 <input type="email"  placeholder="Your Email" name="email" className="form_input"/>
