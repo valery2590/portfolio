65f235f20b2746ebdcb86a9c5b02c6ac411ffa23
valery2590 <valeryfigueroah@gmail.com>
2021-09-11 15:08:00 +0200

    new info added

--- new-portfolio/src/pages/QuestionsPage.js
+++ new-portfolio/src/pages/QuestionsPage.js
@@ -15,16 +15,17 @@ const QuestionsPage = ()=>{
       
             <div className="myHistory__container">
                 
-                <div className="pastHistory_section">
-                    <div className="pastHistory_section_question"> 
+                <div className="history_section">
+                    <div className="pastHistory_section"> 
                         <p className="pastHistory_section_first">Who I was</p>
-                        <p className="pastHistory_section_first">Born in Perú, had the chance to come to Spain</p>
+                        <p className="pastHistory_section_first">Born in Perú, had the chance to come to Spain when I was 10 years old
+                            </p>
                     </div>
                     <div className="pastHistory_section_item">photo</div>
                     <img src={babyGroot} alt="baby_groot" className="question_picture"/>
                 </div>
 
-                <div className="presentHistory_section">
+                <div className="history_section">
                     <div className="presentHistory_section_question">
                         <p className="presentHistory_section_first">Who I am</p>
                         <p className="presentHistory_section_first">Someone who Had the chance to work several years in different tourist companies, since I spent 4 great years of my life at University getting a Tourism Degree, learning languages, business administration</p>
@@ -33,7 +34,7 @@ const QuestionsPage = ()=>{
                     <img src={babyGroot} alt="baby_groot" className="question_picture"/>
                 </div>
 
-                <div className="futureHistory_section">
+                <div className="history_section">
                     <div className="futureHistory_section_question">
                         <p className="futureHistory_section_first">Who I will be</p>
                         <p className="futureHistory_section_first">Someone who Had the chance to work several years in different tourist companies, since I spent 4 great years of my life at University getting a Tourism Degree, learning languages, business administration</p>
