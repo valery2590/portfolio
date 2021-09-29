8f407f1c6e698a8272c5ef2a6eee66686d949ed3
valery2590 <valeryfigueroah@gmail.com>
2021-09-29 16:37:08 +0200

    new info

--- new-portfolio/src/pages/QuestionsPage.js
+++ new-portfolio/src/pages/QuestionsPage.js
@@ -30,7 +30,7 @@ const QuestionsPage = ()=>{
                 
             {question ===  smallTree &&(
                  <div className="history_section">
-                      <div className="pastHistory_section"> 
+                      <div className="text_history_section"> 
                         <p className="question_title_history">Who was I?</p>
                         <p className="question_text_history">Born in Perú, had the chance to come to Spain when I was 10 years old.
                         After 4 years in University, I got my Tourism Degree in Barcelona, this fact helped me to work in several different companies 
@@ -45,7 +45,7 @@ const QuestionsPage = ()=>{
 
  {question ===  middleTree &&(
                     <div className="history_section">
-                <div className="presentHistory_section">
+                <div className="text_history_section">
                     <p className="question_title_history">Who am I?</p>
                     <p className="question_text_history">Working as a Manager had the privilege to learn more about coding since I was trying to make more efficient my duties and my colleagues' duties.
                     I have created tools to improve my daily work. So, last year I decided to push this love for code, I entered a 5 months Full-Stack Bootcamp since I wanted to be a programmer. Here I am now.
@@ -60,7 +60,7 @@ const QuestionsPage = ()=>{
 
                  {question ===  bigTree &&(
                <div className="history_section">
-                <div className="futureHistory_section_question">
+                <div className="text_history_section">
                     <p className="question_title_history">Who will I become?</p>
                     <p className="question_text_history">Hopefully, I will become the same person, however, with more experience. Someone who wants to learn more, to discover more. I do not think there is an end. 
                     And the moment I would think there is one, probably I would have failed. </p>
