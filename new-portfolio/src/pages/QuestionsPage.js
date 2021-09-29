0158dc62248dd36fd383c5fd4371f0ee13185612
valery2590 <valeryfigueroah@gmail.com>
2021-09-29 13:03:06 +0200

    new text included

--- new-portfolio/src/pages/QuestionsPage.js
+++ new-portfolio/src/pages/QuestionsPage.js
@@ -31,7 +31,9 @@ const QuestionsPage = ()=>{
                     <div className="presentHistory_section">
                         <p className="question_title_history">Who am I?</p>
                         <p className="question_text_history">Working as a Manager had the privilege to learn more about coding since I was trying to make more efficient my duties and my colleagues' duties.
-                        I have created tools to improve my daily work. So, last year I decided to push this love for code, I entered a 5 months Full-Stack Bootcamp since I wanted to be a programmer. Here I am now.</p>
+                        I have created tools to improve my daily work. So, last year I decided to push this love for code, I entered a 5 months Full-Stack Bootcamp since I wanted to be a programmer. Here I am now.
+                        Someone who has the courage to change their carreer,
+                        someone who has the guts to start over.</p>
                     </div>
                     <div className="picture_history_section">
                     <img src={middleTree} alt="baby_groot" className="question_picture"/>
@@ -42,8 +44,8 @@ const QuestionsPage = ()=>{
                 <div className="history_section">
                     <div className="futureHistory_section_question">
                         <p className="question_title_history">Who will I become?</p>
-                        <p className="question_text_history">Someone who had the courage to change their carreer,
-                        someone who had the guts to start over. Someone who will study more about codes. I know my right time will come.</p>
+                        <p className="question_text_history">Hopefully, I will become the same person, however, with more experience. Someone who wants to learn more, to discover more. I do not think there is an end. 
+                        And the moment I would think there is one, probably I would have failed. </p>
                     </div>
                     <div className="picture_history_section">
                         <img src={bigTree} alt="big_tree" className="question_picture"/>   
