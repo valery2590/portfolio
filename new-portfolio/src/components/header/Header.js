71f721ec82758e569291168ee4d27e150b505ea4
valery2590 <valeryfigueroah@gmail.com>
2021-10-06 12:35:06 +0200

    header icons modified

--- new-portfolio/src/components/header/Header.js
+++ new-portfolio/src/components/header/Header.js
@@ -16,14 +16,17 @@ const Header = ()=>{
     return (
         <div className="header__container">
             <div className="header_logo_container"><Logo style="logo_header"/></div>
+
             <div className="profile_section">
                 <img src={PhotoProfile} className="profilePicture"  alt="profile_Picture" onClick={()=> history.push("/")}/>
                 <p className="name_title">Valery Figueroa Huamán</p>
                 <p className="position_title">Web Developer</p>
             </div>
-            <div >
-                <div className="headerIcon_container">
-                <a href="https://github.com/valery2590" target="_blank">
+
+            <div className="headerIcon_container">
+               <div className="iconSection">
+
+               <a href="https://github.com/valery2590" target="_blank">
                     <img src={GitIcon} className="iconHeader" alt="git_Icon"/>
                 </a>   
                 <a href="https://wa.me/34680535856" target="_blank"> 
@@ -31,10 +34,11 @@ const Header = ()=>{
                 </a>          
                 <a href="https://www.linkedin.com/in/valery-figueroa-huam%C3%A1n-01517982/" target="_blank">      
                     <img src={LinkdinIcon} className="iconHeader" alt="linkdin_Icon"/>
-                 </a>
-                </div>
+                </a>
+               </div>
+               <ModalSkills style="skills_header" />
+              
                
-                 <ModalSkills style="skills_header" />
             </div>
         </div>
     )
