c8236a51b15a989b39ac18b03dfabe48c1c06435
valery2590 <valeryfigueroah@gmail.com>
2021-10-12 09:41:29 +0100

    new changes

--- new-portfolio/src/components/questions/Questions.js
+++ new-portfolio/src/components/questions/Questions.js
@@ -9,18 +9,19 @@ const Questions = ({className, onClickA, onClickB, onClickC})=>{
     
     return(
                 <div className={className}>
+                    {/* <FontAwesomeIcon icon="fa-solid fa-bars" /> */}
 
                 <div className={click ? "question_clicked": "question"}
                 onClick={onClickA}>
                     <p>Who was I?</p></div>
                     
                 <div className={click ? "question_clicked": "question"} 
-                 onClick={onClickB}>
+                onClick={onClickB}>
                     <p>Who am I?</p>
                 </div>
 
                 <div className={click ? "question_clicked": "question"}
-               onClick={onClickC}>
+                onClick={onClickC}>
                     <p>Who will I become?</p>
                 </div>
 
@@ -30,7 +31,7 @@ const Questions = ({className, onClickA, onClickB, onClickC})=>{
                     </div>
 
                     <div className="question"
-                    onClick={()=> history.push("/projects")}>
+                onClick={()=> history.push("/projects")}>
                     <p>Projects?</p></div>
             </div>
     )
