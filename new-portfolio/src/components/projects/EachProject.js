2d60a2020eaae5a16d273bfa16b358bacbb5c749
valery2590 <valeryfigueroah@gmail.com>
2021-10-24 19:28:00 +0200

    mobile version modified

--- new-portfolio/src/components/projects/EachProject.js
+++ new-portfolio/src/components/projects/EachProject.js
@@ -8,7 +8,8 @@ const EachProject = ({name, link, link2,img, img2}) => {
                 <p className="link_project">{name}</p>
                 <div className="links_images">
                     <a href={link} target="_blank" title="link">
-                        <img src={img} className="headerIconProject" alt="link"/></a>                              
+                        <img src={img} className="headerIconProject" alt="link"/></a>  
+                                                    
                     <a href={link2} target="_blank" title="link2">
                         <img src={img2} className="headerIconProject" alt="link2"/></a> 
                 </div>
