af8c61f6c17fb61b21ae9d6b02a4499cdbfacb46
ValeryFH <valeryfigueroah@gamail.com>
2022-01-26 09:31:16 +0100

    questionary added

--- new-portfolio/src/pages/WhyPage.js
+++ new-portfolio/src/pages/WhyPage.js
@@ -10,28 +10,24 @@ const WhyPage = () => {
     const [show, setShow] = useState(false);
     return (
         <div className="whyPage__container">
-           <Questions className="questions_container_sidebar" 
-            onClickA={()=> history.push("/myhistory")}
-            onClickB={()=> history.push("/myhistory")}
-            onClickC={()=> history.push("/myhistory")}
-            />
+            <Questions className="questions_container_sidebar" 
+                onClickA={()=> history.push("/myhistory")}
+                onClickB={()=> history.push("/myhistory")}
+                onClickC={()=> history.push("/myhistory")}/>
         
             <div className="whyPage_section">
-            <div className='title_WhyPage'>
-                <p>Best way to know WHY you need to hire me</p>
+                <div className='title_WhyPage'>
+                    <p>Best way to know WHY you need to hire me</p>
                 </div>
                 <div className='subtitle_WhyPage'>
-                    <p>image of party</p>
-                    <p>Let's see if you have paid attention to my history</p>
-                    
+                        <p>Let's see if you have paid attention to my history</p>
+                </div>
+                <div className='button_container_WhyPage'>
+                    <button onClick={()=>setShow(true)} className='button_WhyPage'> Let's play</button>
+                    <button onClick={()=>setShow(false)} className='button_WhyPage'> Leave it for another day!</button>
+                </div>
+                <div className='trivia_container_whyPage'> { show === true &&(<Trivia />)}</div>
             </div>
-            <div className='button_container_WhyPage'><button onClick={()=>setShow(true)} className='button_WhyPage'> Let's play</button>
-            <button onClick={()=>setShow(false)} className='button_WhyPage'> Leave it for another day!</button>
-            
-            </div>
-            { show === true &&(<Trivia />)}
-            </div>
-        
         </div>
     );
 };
