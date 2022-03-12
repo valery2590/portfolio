1a88d458f148a0dfadeb7eb2e319f19e172f3baa
ValeryFH <valeryfigueroah@gamail.com>
2022-03-12 15:16:08 +0100

    scroll added

--- new-portfolio/src/components/questions/Questions.js
+++ new-portfolio/src/components/questions/Questions.js
@@ -1,30 +1,52 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
-import "./Questions.css"
+import "./Questions.css";
 
-const Questions = ({className, onClickA, onClickB, onClickC})=>{
-    const history = useHistory();
-    const [click, setClick] = useState(false);
+const Questions = ({ className, onClickA, onClickB, onClickC }) => {
+  const history = useHistory();
+  const [click, setClick] = useState(false);
 
-    
-    return(
-                <div className={className}>
-                <div className="question"
-                    onClick={()=> history.push("/myStory")}>
-                    <p>My Story</p></div>
-                    
-              
-
-                <div className="question"
-                onClick={()=> history.push("/quiz")}>
-                    <p>Quiz</p>
-                    </div>
-
-                    <div className="question"
-                onClick={()=> history.push("/projects")}>
-                    <p>Projects</p></div>
-                </div>
-    )
+const focus = ()=>{
+    if(window.screen.width >= 421){
+          window.scrollTo(0,400);
+    }
 }
 
-export default Questions; 
+ 
+
+  return (
+    <div className={className}>
+      <div
+        className="question"
+        onClick={() => {
+          history.push("/myStory");
+          focus();
+        }}
+      >
+        <p>My Story</p>
+      </div>
+
+      <div
+        className="question"
+        onClick={() => {
+            focus();
+          history.push("/quiz");
+        }}
+      >
+        <p>Quiz</p>
+      </div>
+
+      <div
+        className="question"
+        onClick={() => {
+            focus();
+          history.push("/projects");
+        }}
+      >
+        <p>Projects</p>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
