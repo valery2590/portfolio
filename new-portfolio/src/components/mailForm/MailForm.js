73e46705394901909b790d3df253482d5929251e
Valery <valeryfigueroah@gmail.com>
2023-04-23 13:44:58 +0100

    new states

--- new-portfolio/src/components/mailForm/MailForm.js
+++ new-portfolio/src/components/mailForm/MailForm.js
@@ -1,26 +1,69 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import emailjs from 'emailjs-com'
 import "./MailForm.css"
 import Modal from 'react-modal'
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 
 Modal.setAppElement('#root')
 
 const MailForm = ({ name, src, className, className2 }) => {
 
-    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [confirmationMessage, setConfirmationMessage] = useState('');
+    const [valueInput, setValueInput] = useState({
+        nombre: '',
+        email: '',
+        phone: '',
+        textarea: '',
+    })
+    console.log(valueInput.nombre)
+     //const [inputName, setInputName] = useState('')
+    const inputRef = useRef()
 
+    function handleChangeInput(e) {
+        setValueInput({
+            ...valueInput,
+            [e.target.name] : e.target.value
+       })
+       console.log([e.target.name]+ ': ' + e.target.value)
+       setConfirmationMessage('')
+    }
+
+    function modalFunction (){
+        setModalIsOpen(true)
+        setConfirmationMessage('')
+    } 
     function sendEmail(e) {
         e.preventDefault();
+        let nombrefield = valueInput.nombre;
+        let email = valueInput.email;
+        let phone = valueInput.phone;
+        let textarea = valueInput.textarea;
 
-        emailjs.sendForm('service_lausod2', 'template_abu8jdt', e.target, 'user_iWFLiJoHRNUY2yoUbWCKE')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.target.reset();
-        setModalIsOpen(false)
+          if(nombrefield !== '' & email !== '' & phone !== '' & textarea !== ''){
+            console.log(nombrefield +' '+ email +' '+ phone +' '+ textarea) 
+            emailjs.sendForm('service_lausod2', 'template_abu8jdt', e.target, 'user_iWFLiJoHRNUY2yoUbWCKE')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+            e.target.reset();
+            setConfirmationMessage('Message has been sent correctly')
+            setValueInput({
+                nombre: '',
+                email: '',
+                phone: '',
+                textarea: '',
+            })
+            setTimeout(() => {
+                setModalIsOpen(false)
+            }, 2000);
+        } else {
+            setConfirmationMessage('nothing')
+        }
+
     }
 
     return (
@@ -51,15 +94,19 @@ const MailForm = ({ name, src, className, className2 }) => {
                 <form onSubmit={sendEmail} className="form__container">
 
                     <p className="form_title" id="title">Please fill in to contact me</p>
-                    <input type="text" placeholder="Your Name" name="name" className="form_input" id="input1" />
-                    <input type="email" placeholder="Your Email" name="email" className="form_input" id="input2" />
-                    <input type="tel" pattern="[0-9]{3}[0-9]{3}[0-9]{3}" placeholder="Phone Number" name="number" className="form_input" id="input3" />
-                    <textarea type="text" placeholder="Your Message" name="message" className="form_textarea" id="input4" />
+                    <input type="text" onChange={handleChangeInput} placeholder="Your Name" name="nombre" className="form_input" id="inputName" value={valueInput.nombre}/>
+                    <input type="email" onChange={handleChangeInput}  placeholder="Your Email" name="email" className="form_input" id="inputEmail" value={valueInput.email} />
+                    <input type="phone" onChange={handleChangeInput}  placeholder="Phone Number" name="phone" className="form_input" id="inputTel"  value={valueInput.phone}/>
+                    <textarea type="text" onChange={handleChangeInput}  placeholder="Your Message" name="textarea" className="form_textarea" id="iinputTextarea" value={valueInput.textarea} />
                     <input type="submit" value="Send" className="button_submit" id="button" />
+                    {confirmationMessage && (<span className={confirmationMessage === 'Message has been sent correctly' ? 'message-valid':'message-not-valid'}>{confirmationMessage}</span>)}
+
+                    {/* {confirmationMessage && (<span className='confirmationMessage-text'>{confirmationMessage}</span>)} */}
+
                 </form>
 
             </Modal>
-            <div onClick={() => setModalIsOpen(true)} className="mailform-texto-button">
+            <div onClick={modalFunction} className="mailform-texto-button">
                 {name}
             </div>
         </>
