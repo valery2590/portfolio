33409c420b4399fb63a39c73af9c8bdc50860bec
valery2590 <valeryfigueroah@gmail.com>
2025-01-24 21:17:58 +0000

    updating the whole file

--- new-portfolio/src/components/header/Header.js
+++ new-portfolio/src/components/header/Header.js
@@ -1,11 +1,12 @@
 import { React, useState } from "react";
-import "./Header.css";
+//import "./Header.css";
 import GitIcon from "../../assets/git-icon.svg";
 import WhatsappIcon from "../../assets/whatsapp-symbol.svg";
 import LinkdinIcon from "../../assets/linkedin-icon-2.svg";
 import { useHistory } from "react-router-dom";
 import Questions from "../questions/Questions";
 import cvIcon from "../../assets/cv.svg";
+import styles from "../../styles/generalStyles.module.scss";
 
 const Header = () => {
   const history = useHistory();
@@ -14,30 +15,51 @@ const Header = () => {
 
   return (
     <div className="header__container">
-      <div className="icon_container">
-        <a
-          href="https://github.com/valery2590"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={GitIcon} className="iconHeader" alt="git_Icon" />
-        </a>
-        <a href={whatsappUrl} target="_blank" rel="noreferrer">
-          <img src={WhatsappIcon} className="iconHeader" alt="whatsapp_Icon" />
-        </a>
-        <a
-          href="https://drive.google.com/uc?export=download&id=1pIMFghw77B2UhCLjzBg9SkHB3i-jAxpS"
-          download="cv.valery"
-        >
-          <img src={cvIcon} className="iconHeader" alt="cv_Icon" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/valery-figueroa-huaman-01517982/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={LinkdinIcon} className="iconHeader" alt="linkdin_Icon" />
-        </a>
+      <div className={styles.headerIconSection}>
+        <ul className={styles.navContainer}>
+          <li className={styles.navLiIcons}>
+            {" "}
+            <a
+              href="https://github.com/valery2590"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={GitIcon} className={styles.navIconImg} alt="git_Icon" />
+            </a>
+          </li>
+          <li className={styles.navLiIcons}>
+            {" "}
+            <a href={whatsappUrl} target="_blank" rel="noreferrer">
+              <img
+                src={WhatsappIcon}
+                className={styles.navIconImg}
+                alt="whatsapp_Icon"
+              />
+            </a>
+          </li>
+          <li className={styles.navLiIcons}>
+            {" "}
+            <a
+              href="https://drive.google.com/uc?export=download&id=1pIMFghw77B2UhCLjzBg9SkHB3i-jAxpS"
+              download="cv.valery"
+            >
+              <img src={cvIcon} className={styles.navIconImg} alt="cv_Icon" />
+            </a>
+          </li>
+          <li className={styles.navLiIcons}>
+            <a
+              href="https://www.linkedin.com/in/valery-figueroa-huaman-01517982/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={LinkdinIcon}
+                className={styles.navIconImg}
+                alt="linkdin_Icon"
+              />
+            </a>
+          </li>
+        </ul>
       </div>
 
       <div
@@ -46,7 +68,7 @@ const Header = () => {
           history.push("/");
         }}
       >
-       <p className="name-title-text"> Valery Figueroa Huamán</p>
+        <p className="name-title-text"> Valery Figueroa Huamán</p>
       </div>
 
       <div className="iconSection">
